3aca4be44d45ec51028c2d26b84725544af09415
seehoon <flytofree@p2ach.ai>
2025-11-17 13:20:52 +0900

    디자인 요소 추가

--- app/src/main/java/ai/p2ach/p2achandroidvision/views/fragments/FragmentMDM.kt
+++ app/src/main/java/ai/p2ach/p2achandroidvision/views/fragments/FragmentMDM.kt
@@ -2,7 +2,8 @@ package ai.p2ach.p2achandroidvision.views.fragments
 
 import ai.p2ach.p2achandroidlibrary.base.fragments.BaseFragment
 import ai.p2ach.p2achandroidlibrary.utils.Log
-import ai.p2ach.p2achandroidvision.databinding.FragmentCameraBinding
+
+import ai.p2ach.p2achandroidvision.databinding.FragmentMdmBinding
 import ai.p2ach.p2achandroidvision.utils.toUiItems
 import ai.p2ach.p2achandroidvision.viewmodels.MdmViewModel
 import ai.p2ach.p2achandroidvision.views.fragments.adapters.MDMAdapter
@@ -14,7 +15,7 @@ import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-class FragmentMDM : BaseFragment<FragmentCameraBinding>(){
+class FragmentMDM : BaseFragment<FragmentMdmBinding>(){
 
 
     private val mdmViewModel : MdmViewModel by viewModel()
